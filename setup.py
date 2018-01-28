840b4edac83738c163fe33e778346e9fe0e6cee6
Jan Sourek <jan.sourek@gmail.com>
2018-01-28 16:44:03 +0100

    fixed dateutil dependency

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     packages=['elastic_connect'],
     license='MIT',
     description='Elasticsearch "ORM"',
-    install_requires=['elasticsearch', 'dateutil', 'requests']
+    install_requires=['elasticsearch', 'python-dateutil', 'requests']
 )
