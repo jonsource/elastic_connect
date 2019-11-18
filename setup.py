ddabef4133a413488b1c726fec5b1f80c37a667c
Jan Sourek <jan.sourek@gmail.com>
2019-11-18 23:07:19 +0100

    Pushed version to v0.2.3-b

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='elastic_connect',
-    version='0.2.1',
+    version='0.2.3-b',
     author='Jan Sourek',
     author_email='jan.sourek@gmail.com',
     packages=['elastic_connect', 'elastic_connect.data_types'],
