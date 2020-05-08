3087d8ca226b354ced2ce6df809c2ed5056dc7e3
Jan Sourek <jan.sourek@gmail.com>
2020-05-08 20:08:22 +0200

    Stamped model

---
+++ elastic_connect/advanced_model.py
@@ -0,0 +1,136 @@
+import elastic_connect
+import elastic_connect.data_types as data_types
+import elastic_connect.data_types.base
+import logging
+from datetime import datetime
+from .base_model import Model
+from elasticsearch.exceptions import NotFoundError
+from contextlib import contextmanager
+
+logger = logging.getLogger(__name__)
+
+
+NONE = 0
+WITH = 1
+ONLY = 2
+
+
+class StampedModel(Model):
+    __slots__ = ('created_at', 'updated_at', 'deleted')
+
+    thrash_handling = NONE
+
+    @classmethod
+    def model_mapping(cls, **args):
+        """
+        Creates a Mapping with given parameters.
+
+        Automatically adds the id field as Keyword if not present
+        Automatically adds created_at and updated_at as Date
+        Automatically adds deleted as Boolean
+
+        :param **args: field_name=data_type pairs describing the fields
+            of this model.
+
+        :return: Mapping
+        """
+
+        mapping = super().model_mapping(**args)
+        mapping.add_field('created_at', data_types.Date())
+        mapping.add_field('updated_at', data_types.Date())
+        mapping.add_field('deleted', data_types.Boolean())
+        return mapping
+
+    @classmethod
+    def create(cls, **kw) -> 'StampedModel':
+        now = datetime.now()
+        kw['created_at'] = now
+        kw['updated_at'] = now
+        kw['deleted'] = False
+        instance = super().create(**kw)
+        return instance
+
+    def save(self) -> 'StampedModel':
+        now = datetime.now()
+        self.updated_at = now
+        return super().save()
+
+    def delete(self, force=False):
+        if force:
+            return super().delete()
+
+        self.deleted = True
+        ret = self.save()
+        return
+
+    @classmethod
+    def restore(cls, id) -> 'StampedModel':
+        with cls.thrashed():
+            instance = cls.get(id)
+        instance.deleted = False
+        instance.save()
+        return instance;
+
+    @classmethod
+    def get(cls, id):
+        result = cls.find_by(size=1, _id=id)
+        if not len(result):
+            raise NotFoundError()
+        if len(result) > 1:
+            raise IntegrityError("Get returned multiple items, should return one!")
+        return result[0]
+
+    @classmethod
+    def all(cls, size=100, sort=None):
+        return cls.find_by(size=size, sort=sort)
+
+    @classmethod
+    def find_by(cls,
+                size=100,
+                sort=None,
+                search_after=None,
+                query=None,
+                **kw):
+
+        if query:
+            # add deleted=false to query
+            pass
+        else:
+            kw = {**kw, **cls._thrashed_kw()}
+        return super().find_by(size=size, sort=sort,
+            search_after=search_after, query=query, **kw)
+
+    @classmethod
+    def _thrashed_kw(cls):
+        if(cls.thrash_handling == WITH):
+            return {}
+        if(cls.thrash_handling == ONLY):
+            return {'deleted': True}
+        return {'deleted': False}
+
+    @classmethod
+    @contextmanager
+    def thrashed(cls):
+        return cls._thrashed(WITH)
+
+    @classmethod
+    @contextmanager
+    def thrashed_only(cls):
+        return cls._thrashed(ONLY)
+        
+    @classmethod
+    def _thrashed(cls, handling):
+        orig = cls.thrash_handling
+        cls.thrash_handling = handling
+        try:
+            yield cls
+        finally:
+            cls.thrash_handling = orig
+
+
+class VersionedModel(Model):
+    pass
+
+
+class AuditedModel(Model):
+    pass
---
+++ tests/test_stamped_model.py
@@ -0,0 +1,232 @@
+import pytest
+from elastic_connect.base_model import Model
+from elastic_connect.advanced_model import StampedModel
+from elastic_connect import advanced_model
+import elastic_connect
+from elastic_connect.data_types import Keyword, Date
+import elasticsearch.exceptions
+from datetime import datetime, timedelta
+from elasticsearch.exceptions import NotFoundError
+
+
+@pytest.fixture(scope="module")
+def fix_stamped_model(request):
+
+    class StampedOne(StampedModel):
+        __slots__ = ('value', )
+
+        _meta = {
+            '_doc_type': 'stamped_one'
+        }
+        _mapping = StampedModel.model_mapping(value=Keyword())
+
+    es = elastic_connect.get_es()
+    indices = elastic_connect.create_mappings(model_classes=[StampedOne])
+    assert es.indices.exists(index=StampedOne.get_index())
+
+    yield StampedOne
+
+    if request.config.getoption("--index-noclean"):
+        print("** not cleaning")
+        return
+
+    elastic_connect.delete_indices(indices=indices)
+    assert not es.indices.exists(index=StampedOne.get_index())
+
+
+@pytest.fixture(autouse=True)
+def delete_all_instances(fix_stamped_model):
+    yield
+
+    with fix_stamped_model.thrashed():
+        instances = fix_stamped_model.all()
+
+    for instance in instances:
+        instance.delete(force=True)
+
+    fix_stamped_model.refresh()
+
+    with fix_stamped_model.thrashed():
+        assert len(fix_stamped_model.all()) == 0
+
+
+def test_mapping(request, fix_stamped_model):
+    StampedOne = fix_stamped_model()
+    expected_mapping = {'created_at': {'type': 'date'},
+                        'updated_at': {'type': 'date'},
+                        'deleted': {'type': 'boolean'},
+                        'value': {'type': 'keyword'}}
+
+    mapping = StampedOne.get_es_mapping()
+    assert mapping == expected_mapping
+
+
+def test_create(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    now = datetime.now()
+
+    instance = cls.create(value='value1')  # type: StampedModel
+
+    assert instance.id is not None
+    assert instance.created_at == now
+    assert instance.updated_at == now
+
+    cls.refresh()
+
+    es = elastic_connect.get_es()
+    es_result = es.get(index=cls.get_index(), doc_type=cls.get_doctype(), id=instance.id)
+
+    assert es_result['found'] == True
+    assert es_result['_source'] == {
+        'value': 'value1',
+        'created_at': now.isoformat(),
+        'updated_at': now.isoformat(),
+        'deleted': False
+    }
+
+
+def test_update(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    created = datetime.now()
+
+    instance = cls.create(value='value1')  # type: StampedModel
+    cls.refresh()
+
+    loaded = cls.get(instance.id)
+    assert loaded.created_at == created
+    assert loaded.updated_at == created
+
+    freezer.tick(delta=timedelta(seconds=10))
+
+    updated = created + timedelta(seconds=10)
+    
+    loaded.value = "dva"
+    loaded.save()
+    cls.refresh()
+
+    loaded = cls.get(instance.id)
+    assert loaded.created_at == created
+    assert loaded.updated_at == updated
+
+
+def test_get(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    now = datetime.now()
+
+    instance = cls.create(value='value1')  # type: StampedModel
+    cls.refresh()
+
+    #loaded = cls.get(instance.id)
+    loaded = cls.get(instance.id)
+    
+    assert loaded.created_at == now
+    assert loaded.updated_at == now
+
+
+def test_get_nonexistant(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    with pytest.raises(elasticsearch.exceptions.NotFoundError):
+        instance = cls.get("badbadId")
+
+
+def test_all(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    instance1 = cls.create(value='value1')  # type: StampedModel
+    instance2 = cls.create(value='value1')  # type: StampedModel
+    instance3 = cls.create(value='value1')  # type: StampedModel
+    cls.refresh()
+
+    assert len(cls.all()) == 3
+
+
+def test_delete_single(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    instance = cls.create(value='value1')  # type: StampedModel
+    cls.refresh()
+
+    assert cls.get(instance.id).id == instance.id
+
+    instance.delete()
+    cls.refresh()
+
+    with pytest.raises(elasticsearch.exceptions.NotFoundError):
+        cls.get(instance.id)
+
+    assert len(cls.all()) == 0
+
+
+def test_delete(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    instance1 = cls.create(value='value1')  # type: StampedModel
+    instance2 = cls.create(value='value1')  # type: StampedModel
+    instance3 = cls.create(value='value1')  # type: StampedModel
+    cls.refresh()
+
+    assert len(cls.all()) == 3
+
+    instance3.delete()
+    cls.refresh()
+
+    assert len(cls.all()) == 2
+    with pytest.raises(elasticsearch.exceptions.NotFoundError):
+        cls.get(instance3.id)
+
+
+def test_restore(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    instance = cls.create(value='value1')  # type: StampedModel
+    instance.delete()
+    cls.refresh()
+
+    with pytest.raises(elasticsearch.exceptions.NotFoundError):
+        cls.get(instance.id)
+
+    cls.restore(instance.id)
+    cls.refresh()
+    
+    assert cls.get(instance.id).id == instance.id
+
+    with pytest.raises(elasticsearch.exceptions.NotFoundError):
+        cls.restore('badbadId')
+
+
+def test_contextmanager_thrashed(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    assert cls.thrash_handling == advanced_model.NONE
+
+    inst = cls(value='value_some')
+    assert inst.thrash_handling == advanced_model.NONE
+
+    with cls.thrashed():
+        assert cls.thrash_handling == advanced_model.WITH
+        assert inst.thrash_handling == advanced_model.WITH
+
+    assert cls.thrash_handling == advanced_model.NONE    
+    assert inst.thrash_handling == advanced_model.NONE
+
+
+def test_thrashed(freezer, fix_stamped_model):
+    cls = fix_stamped_model
+
+    instance1 = cls.create(value='value1')  # type: StampedModel
+    instance2 = cls.create(value='value1')  # type: StampedModel
+    instance3 = cls.create(value='value1')  # type: StampedModel
+    instance3.delete()
+    cls.refresh()
+
+    assert len(cls.all()) == 2
+    
+    with cls.thrashed():
+        assert len(cls.all()) == 3
+
+    with cls.thrashed_only():
+        assert len(cls.all()) == 1
